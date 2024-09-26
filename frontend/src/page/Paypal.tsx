1422a15fa1ad98dbc274805e8b3c323a4b72a3fb
Totragool <totragool24@gmail.com>
2024-09-26 12:31:41 +0700

    26/9/2567 12:31

--- frontend/src/page/Paypal.tsx
+++ frontend/src/page/Paypal.tsx
@@ -3,7 +3,8 @@ import { Layout, Divider, List, Typography, Button, Card, Form, Input, Flex } fr
 import { usePaymentService } from './paymentService';
 import { useBookingService } from './bookingService';
 import { Payment } from './payment.interface';
-// import imageSrc from './assets/Screenshot 2024-09-19 023621.png';
+import imageSrc from './Screenshot 2024-09-19 023621.png';
+
 
 
 const { Header, Footer, Content } = Layout;
@@ -216,7 +217,7 @@ const Paypal: React.FC = () => {
         <div className="container">
         <div className='topbar'>
               <div style={headerContainerStyle}>
-                  {/* <img src={imageSrc} alt="description" style={{ width: '10%', height: '10%',marginRight: '5%' }} /> */}
+                  <img src={imageSrc} alt="description" style={{ width: '10%', height: '10%',marginRight: '5%' }} /> 
                   <Flex gap="small" wrap>
                     <Button type="primary" style={buttonStyle}>Home</Button>
                     <Button type="primary" style={buttonStyle}>Fight</Button>
